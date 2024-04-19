ff46136e493db108d0c617dc389f6b59ae55f3e9
Juan Soto <juan.a.soto.h@gmail.com>
2024-04-19 16:41:09 -0400

    Added annotations

--- Effect_size_plot.R
+++ Effect_size_plot.R
@@ -8,16 +8,20 @@ if (!require("pacman"))
 pacman::p_load(reshape2, ggplot2, ggplotify, gridExtra, ggpubr)
 
 #CTmax --------------------------------------------------------------------------
+# Load data of CTmax and genotype per snp and snps effect size information 
 CTmax_genot<-read.table("C:/Users/Juan/Documents/Postgrado/LAB_Bio_integrativa/Papers/Soto_etal_2022_TDT_DGRP/scripts/Data/CTmax_snp_geno.csv", header = T,sep =";",check.names = F)
 CTmax_snp_genes <- read.table("C:/Users/Juan/Documents/Postgrado/LAB_Bio_integrativa/Papers/Soto_etal_2022_TDT_DGRP/scripts/Data/CTmax_SNP.csv", header = T,sep =";")
 
+# prepare data
 for (i in 3:ncol(CTmax_genot)){
   CTmax_genot[,i] <- as.factor(CTmax_genot[,i]) 
 }
 
 CTmax_genot_melt <- melt(CTmax_genot[,-1])
+# make empty list to store graphs
 lista_graficos <- list()
 
+# make box plot per snp and store it in list
 for (i in 1:8){
   propiedades_snp <- subset(CTmax_snp_genes,ID==colnames(CTmax_genot_melt[i]))
   grafico <- ggplot(subset(CTmax_genot_melt,!is.na(CTmax_genot_melt[,i])), aes(x=subset(CTmax_genot_melt[,i],!is.na(CTmax_genot_melt[,i])), y= value, color=subset(CTmax_genot_melt[,i],!is.na(CTmax_genot_melt[,i])))) +
@@ -35,6 +39,7 @@ for (i in 1:8){
   lista_graficos[[nombre_grafico]] <- grob_grafico
 }
 
+# arrange graphs
 effect_ctmax_plot <- grid.arrange(grobs=c(c(lista_graficos[1:8])), ncol=4,left=("CTmax(°C)"))
 
 #Z ------------------------------------------------------------------------------
@@ -68,7 +73,5 @@ for (i in 1:8){
 
 effect_z_plot <- grid.arrange(grobs=c(c(lista_graficos[1:8])), ncol=4,left=("z(°C)"))
 
-
-png(file="Effect_box.png",width=3000, height=2500, res = 300)
+# arrange graphs for CTmax and z
 ggarrange(effect_ctmax_plot, effect_z_plot, labels=c("A","B"), ncol=1, nrow=2)
-dev.off()
