ef7e6d855b706266f9da5b79cde23242b9acc079
Juan Soto <juan.a.soto.h@gmail.com>
2025-02-23 09:52:33 -0300

    update

--- LMMs.R
+++ LMMs.R
@@ -170,7 +170,7 @@ anova(male3940.model)
 
 
 # CALCULATE CTMAX AND Z FROM ORIGINAL DATA ------------------------------------
-source("C:/Users/Juan/Documents/Postgrado/LAB_Bio_integrativa/Papers/Soto_etal_2022_TDT_DGRP/TTL-DGRP/Thermal_landscape.R")
+source("Thermal_landscape.R")
 TDT_table <- TDT(data)
 TDT_table$sex <- as.factor(TDT_table$sex)
 
